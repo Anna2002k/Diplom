8ef498769966105593febddad0eba3ed53dd16c4
Your Name <you@example.com>
2022-05-18 11:14:06 +0500

    18.05 11:14

--- main.js
+++ main.js
@@ -19,12 +19,27 @@ app.get('/', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style >
+	h1 {
+		margin-left: 30px;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 			<h1>Основы теоретической физики</h1>
-			<span>Содержание</span> <a href="/search">Поиск</a>
-			<hr/>
+			<hr>
+			<h6><span>Содержание |</span> <a href="/search">Поиск</a></h6>
 			<ul>
 				${content.map(thema => `<div><a href="/content-thema/${thema.id}">${thema.theme}</a></div>`).join('')}
 			</ul>
@@ -41,18 +56,42 @@ app.get('/content-thema/:theme_id', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+	<style >
+	h1 {
+		margin-left: 30px;
+		margin-top: 0%;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	p {
+		text-align:right;
+		margin-bottom: 0%;
+		margin-right: 2%;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-			<h1>Тема: ${thema.theme} <button><a href="/thema/${theme_id}">Редактировать</a></button></h1>
-			<a href="/">Содержание</a> 
+		<p><button><a href="/thema/${theme_id}" >Редактировать название темы</a></button></p>	
+		<h1>Тема: ${thema.theme} </h1>
+			<h6><a href="/">Содержание</a> </h6>
 			<ul class="themes">
-			${themes.map(contents => `<div>${contents.text}  <a href="/contents/${contents.id}">редактировать</a></div>`).join('')}
+			${themes.map(contents => `<div>${contents.text}  <p><a href="/contents/${contents.id}">Редактировать содержимое темы</a></p></div>`).join('')}
 			</ul>
 			</body>
 	</html>`);
 });
 ////////////
+
 app.get('/thema/:theme_id', async function(req, res) {
 	const { theme_id } = req.params;
 	const [[thema]] = await pool.query('SELECT * FROM content WHERE id = ?',theme_id);
@@ -61,11 +100,27 @@ app.get('/thema/:theme_id', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style >
+	h1 {
+		margin-left: 30px;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		
 		<h1>Редактирование темы: ${thema.theme} <form method="post" action="/thema/${theme_id}/remove"></form></h1>
-		<a href="/">Содержание</a>
+		<h6><a href="/">Содержание</a></h6>
 		<form method="post" action="/thema/${theme_id}">
 			<div class="mb-3">	
 				<textarea class="form-control" id="exampleFormControlTextarea1" rows="5" type="text" name="theme" placeholder="Название темы" >${thema.theme}</textarea>
@@ -110,11 +165,26 @@ app.get('/contents/:contents_id', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style >
+	h1 {
+		margin-left: 30px;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 		<h1>Редактирование содержимого темы ${thema.theme} <form method="post" action="/contents/${contents_id}/remove"></form></h1>
-		<a href="/">Содержание</a>
+		<h6><a href="/">Содержание</a></h6>
 		<form method="post" action="/contents/${contents_id}">
 		<div class="mb-3">	
 			<textarea class="form-control" id="exampleFormControlTextarea1" rows="25" type="text" name="text" placeholder="Содержимое темы"  >${contents.text}</textarea>
@@ -136,18 +206,38 @@ app.get('/search', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style >
+	h1 {
+		margin-left: 30px;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	form {
+		margin-top:10px;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-			<h1>Поиск темы</h1>
-			<a href="/">Содержание</a> <span>Поиск</span>
-			<hr/>
+			<h1>Поиск темы</h1>			
+			<hr>
+			<h6><a href="/">Содержание |</a> <span>Поиск</span></h6>
+			<ul>
 			<form method="get" action="/search">
 				<input type="text" name="thema_query" placeholder="Поисковой запрос" value="${thema_query ? thema_query : ''}"/>
 				<button type="submit">Применить</button>
 			</form>
 			Найдено: ${themes.length}
 				${themes.map(contents => `<div>${contents.theme}</div>`).join('')}
+			</ul>
 		</body>
 	</html>`);
 });
@@ -173,6 +263,21 @@ app.get('/search-dynamic', async function(req, res) {
 	<html>
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style >
+	h1 {
+		margin-left: 30px;
+	}
+	ul {
+		border: 1px solid #a7d7f9;
+	}
+	h6 {
+		margin-left: 30px;
+		margin-bottom: 0%;
+		margin-right: 83%;
+    	background: linear-gradient(to top, #E6E6FA,#FFFFFF);
+    	padding: 10px;
+	}
+	</style>
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -185,7 +290,7 @@ app.get('/search-dynamic', async function(req, res) {
 			}
 			</script>
 			<h1>Поиск темы</h1>
-			<a href="/">Содержание</a> <span>Поиск</span>
+			<h6><a href="/">Содержание |</a> <span>Поиск</span></h6>
 			<hr/>
 			<form method="get" action="/search">
 				<input type="text" name="thema_query" oninput="javascript:getNewData(this)" placeholder="Поисковой запрос" value="${thema_query ? thema_query : ''}"/>
@@ -196,6 +301,7 @@ app.get('/search-dynamic', async function(req, res) {
 		</body>
 	</html>`);
 });
+
 
 
 app.listen(80,function() {      //запуск сервера
