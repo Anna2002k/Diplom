ec648adac123c5d9b695f666c46b445be3b9b324
Your Name <you@example.com>
2022-06-02 08:31:29 +0500

    02.06

--- main.js
+++ main.js
@@ -248,7 +248,6 @@ app.get('/search', async function(req, res) {
 	</head>
 		<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-		<h1>Тема: </h1>
 			<h6><a href="/">Содержание</a> </h6>
 			<ul>
 			<form method="get" action="/search">
